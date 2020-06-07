8db2550e7a3681a8c6893acc1c2b00c39173be59
mbedded <github@mbuege.com>
2020-06-07 12:37:05 +0200

    Refactor parameters

--- CoLib.Common/Extensions/StringExtensions.cs
+++ CoLib.Common/Extensions/StringExtensions.cs
@@ -12,32 +12,32 @@ namespace CoLib.Common.Extensions {
     ///   This method tries to Trim a string.
     ///   If the given parameter is null, an empty string "" will be returned.
     /// </summary>
-    /// <param name="xString">The string which should be trimmed</param>
+    /// <param name="input">The string which should be trimmed</param>
     /// <returns>xString.Trim() or "" if xString is null.</returns>
-    public static string TrimOrEmpty(this string xString) {
-      return string.IsNullOrEmpty(xString) ? "" : xString.Trim();
+    public static string TrimOrEmpty(this string input) {
+      return string.IsNullOrEmpty(input) ? "" : input.Trim();
     }
 
     /// <summary>
     ///   Converts a given string to a Base64 string by given encoding.
     /// </summary>
-    /// <param name="xInput">String to change to Base64</param>
-    /// <param name="xEncoding">Encoding for transformation</param>
+    /// <param name="input">String to change to Base64</param>
+    /// <param name="encoding">Encoding for transformation</param>
     /// <returns>Returns a string containing the Base64 converted result</returns>
-    public static string ToBase64(this string xInput, Encoding xEncoding) {
-      byte[] inputAsBytes = xEncoding.GetBytes(xInput);
+    public static string ToBase64(this string input, Encoding encoding) {
+      byte[] inputAsBytes = encoding.GetBytes(input);
       return Convert.ToBase64String(inputAsBytes);
     }
 
     /// <summary>
     ///   Converts a given Base64 string back to normal by given encoding.
     /// </summary>
-    /// <param name="xBase64String">String to change back</param>
-    /// <param name="xEncoding">Encoding for transformation</param>
+    /// <param name="base64String">String to change back</param>
+    /// <param name="encoding">Encoding for transformation</param>
     /// <returns>Returns the normal string by given Base64 input</returns>
-    public static string FromBase64(this string xBase64String, Encoding xEncoding) {
-      byte[] decodedAsBytes = Convert.FromBase64String(xBase64String);
-      return xEncoding.GetString(decodedAsBytes);
+    public static string FromBase64(this string base64String, Encoding encoding) {
+      byte[] decodedAsBytes = Convert.FromBase64String(base64String);
+      return encoding.GetString(decodedAsBytes);
     }
 
   }
